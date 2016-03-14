896fbf4657bf71c3aa92e1510108657621f578c1
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2016-03-14 00:04:04 -0400

    Starting development of AFE configuration module
    Configuration container is currently just a port list
    Implemented the timer for the AFE hardware reset pin
    Put together initial retriggerable output-only SPI module

---
+++ V/afe_configure.v
@@ -0,0 +1,12 @@
+module afe_configure
+       (
+         input clk, reset_n,
+         input miso,
+         output pdn,
+         output cs_n,
+         output mosi,
+         output device_reset,
+         output configure_done
+       );
+
+endmodule
