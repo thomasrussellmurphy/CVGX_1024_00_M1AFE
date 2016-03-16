f24ddb4b0d629099efb77f7e2575f8f6d16b5f2b
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2016-03-16 17:47:31 -0400

    Switch to externally initiallized ROM data
    This also creates a synthesized ROM to store the memory rather than a decoder

--- V/afe_command_rom.v
+++ V/afe_command_rom.v
@@ -12,52 +12,23 @@ module afe_command_rom
          output [ 23: 0 ] command
        );
 
-reg [ 7: 0 ] address_reg;
+// Memory variable
+reg [ 23: 0 ] rom[ 7: 0 ];
 
-// Combinatorial register, won't cause an additional cycle of delay
 reg [ 23: 0 ] command_reg;
 assign command = command_reg;
 
-// ROM controller
-always @( posedge clk or negedge reset_n ) begin
-  if ( ~reset_n )
-  begin
-    address_reg <= 5'b0;
-  end else
-  begin
-    address_reg <= address;
-  end
+// ROM initialization
+initial
+begin
+  // Read hex memory from file
+  // Relative path from this Verilog file
+  $readmemh( "../rom_data/afe_configuration_rom_data.txt", rom );
 end
 
-// ROM data and declaration
-always @( * ) begin
-  case ( address_reg )
-    // AFE has come out of reset
-
-    // Turn on LVDS mode for Tx and Rx, SDOUT as output
-    8'h00:
-      command_reg = 24'h1_20A_0E;
-    // Turn on duty cycle correction for ADC channel A
-    8'h01:
-      command_reg = 24'h1_0DB_01;
-    // Turn on duty cycle correction for ADC channel B
-    8'h02:
-      command_reg = 24'h1_0F2_08;
-    // Set Tx interface to two-wire
-    8'h03:
-      command_reg = 24'h1_30B_80;
-    // Set MASTER_OVERRIDE_TX on
-    8'h04:
-      command_reg = 24'h1_30C_04;
-    // Set MASTER_OVERRIDE_RX and Rx interface to two-wire
-    8'h05:
-      command_reg = 24'h1_33A_82;
-    // Done
-    8'h06:
-      command_reg = 24'h0_000_00;
-    default:
-      command_reg = 24'h0_000_00;
-  endcase
+// ROM controller
+always @( posedge clk or negedge reset_n ) begin
+  command_reg <= rom[ address ];
 end
 
 endmodule
